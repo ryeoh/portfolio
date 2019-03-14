16abd21c6a7b9cba3884eebd23404145c6aa8bfb
Rachel Oh <rachelyjo@gmail.com>
2019-03-14 16:54:28 -0700

    fix js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // smooth scroll affect
 function scrollPage() {
-    $('a').on('click', function(event) {
+    $('.scroll').on('click', function(event) {
         event.preventDefault();
         $('body, html').animate({
          scrollTop: $(this.hash).offset().top - 20}, 1000)
